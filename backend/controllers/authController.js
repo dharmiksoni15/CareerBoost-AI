4017bfbd1ebc10e6ce2fdbe6e2225edfdcd5e8a9
Dharmik Soni <sonidharmik21@gmail.com>
2026-04-14 23:39:24 +0530

    feat: add register validation and store new user in MongoDB with duplicate email check

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,22 +1,51 @@
+const User = require("../models/User");
+
 const registerUser=async(req,res)=>{
     try {
         const {name,email,password}=req.body;
 
-        res.status(200).json({
+        // check required fields 
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"all fields are required",
+            });
+        }
+
+        // check if user already exists
+        const existingUser=await User.findOne({ email });
+
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"User already exists with this email",
+            });
+        }
+
+        // create a new user
+        const newUser=await User.create({
+            name,
+            email,
+            password,
+        });
+
+        res.status(201).json({
             success:true,
-            message:"Register API is working",
-            data:{
-                name,
-                email,
-                password,
+            message:"User Registered Succesfully",
+            user:{
+               id:newUser._id,
+               name:newUser.name,
+               email:newUser.email,
             },
         });
     } catch (error) {
+        console.log("Register Error",error);
+        
         res.status(500).json({
             success:false,
             message:"server Error",
         });
-    }
+    };
 };
 
 module.exports={
