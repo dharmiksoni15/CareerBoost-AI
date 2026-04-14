d1dbfb69d45b6a8c5f79424d7dd40e8e85687570
Dharmik Soni <sonidharmik21@gmail.com>
2026-04-14 22:48:26 +0530

    feat: add auth routes using Express Router and mount at /api/auth for modular API structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB=require("./config/db");
+const authRoutes=require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -8,11 +9,14 @@ const app = express();
 
 connectDB();
 
+
 app.get("/", (req, res) => {
   res.send("CareerBoost-AI backend is running...");
 });
 
-const PORT = 5000;
+app.use("/api/auth",authRoutes);
+
+const PORT = process.env.PORT||5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
